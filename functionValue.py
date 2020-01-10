a48cff7fd1085334300190f09b8afd1537ba29d8
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-01-09 22:05:21 -0800

    functionValue

---
+++ functionValue.py
@@ -0,0 +1,16 @@
+# function with two input
+def multiple(b, a=3):
+    return a * b
+
+
+print(multiple(5, 7))
+print(multiple(7))
+print(multiple(9, a=2))
+
+
+# function with two input
+def add(a, b, c=90):
+    return a + b + c
+
+
+print(add(2, c=1, b=9))
